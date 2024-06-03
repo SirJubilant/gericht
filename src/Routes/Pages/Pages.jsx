35181daaf4c88f694e481b65158a6a85cbbe602c
Jubilant <Jubilantagida@gmail.com>
2024-06-03 12:28:37 +0100

    updates to the gallery section

--- src/Routes/Pages/Pages.jsx
+++ src/Routes/Pages/Pages.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import Button from '../../components/Button/Button';
 import './pages.css';
+import { useState } from 'react';
 
 const getFoodCategories = async () => {
   const resp = await axios.get(
@@ -11,28 +12,54 @@ const getFoodCategories = async () => {
     return resp.data.categories;
 };
 
+
+   function SearchBar({ userText, onUserTextChange }) {
+    return (
+      <form>
+        <input type="text" 
+        placeholder='Search Meals' 
+        value={userText}
+        onChange={(e) => onUserTextChange(e.target.value)}
+        />
+      </form>
+    );
+  }
+
+
 const DisplayFoodCaregories = () => {
+  const [userText, setUserText] = useState('');
   const {
     data: categories,
     error,
     isLoading,
-  } = useQuery("postData", getFoodCategories);
+  } = useQuery("getFoodCategories", getFoodCategories);
   if (isLoading) return <p>Fetching your meals...</p>
   if (error) return <p>An console.error occurred: {error.message}</p>
 
+  const filteredCategories = categories.filter(category => 
+    category.strCategory.toLowerCase().includes(userText.toLowerCase())
+    );
+
+
 
     return (
-    <section className='page'>
-      {categories.map((category) => (
+    <section> 
+       <SearchBar 
+       userText={userText}
+       onUserTextChange={setUserText}
+       />
+    <div className='page'>
+      {filteredCategories.map((category) => (
+        
         <Button 
         key={category.idCategory} 
         buttonText={category.strCategory}
         className="button"
-        >
+        />
 
-        </Button>
       ))}
-    </section>
+    </div>
+      </section>
   )
 };
 
