f5fde424706fe3a85d03de5c04b4378ff4988b00
Jubilant <Jubilantagida@gmail.com>
2024-06-26 16:40:53 +0100

    Fixed production bug

--- src/components/assets.jsx
+++ src/components/assets.jsx
@@ -1,4 +1,4 @@
-import vert_line from "../assets/line.png";
+import vert_line from "../assets/Line.png";
 import hero_img from "../assets/heroImg.webp";
 import r_spoon from "../assets/r-spoon.svg";
 import y_line from "../assets/vertical-line.png";
